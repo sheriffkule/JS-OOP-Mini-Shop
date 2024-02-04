f9a59ca1fac7e5d7a5e8030aeb818159f5089049
sheriffkule <zeljko225@yahoo.com>
2024-02-04 14:57:30 +0100

    Using & "connecting" multiple classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,8 +12,31 @@ class Product {
     }
 }
 
-const productList = {
-    products: [
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+            <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
+            <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+            </div>
+            </div>
+            `;
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
         new Product(
             'A Pillow',
             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPWZ1SG02SGwRyuY3CDSlbR72g8cpRosEwpp2LiCfbbA&s',
@@ -26,29 +49,20 @@ const productList = {
             'A carpet that you might like - or not.',
             89.99
         ),
-    ],
+    ];
+    constructor() {}
     render() {
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-            <div>
-            <img src="${prod.imageUrl}" alt="${prod.title}">
-            <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-            </div>
-            </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
-    },
-};
+    }
+}
 
+const productList = new ProductList();
 productList.render();
