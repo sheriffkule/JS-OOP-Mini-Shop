323b3806de398ca54d43dddf3a0a65f0108aba6a
sheriffkule <zeljko225@yahoo.com>
2024-02-05 21:14:57 +0100

    overriding methods and  the super() constructor execution, order & this

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -20,9 +20,14 @@ class ElementAttribute {
 }
 
 class Component {
-    constructor(renderHookId) {
+    constructor(renderHookId, shouldRender = true) {
         this.hookId = renderHookId;
+        if (shouldRender) {
+            this.render();
+        }
     }
+
+    render() {}
 
     createRootElement(tag, cssClasses, attributes) {
         const rootElement = document.createElement(tag);
@@ -79,8 +84,9 @@ class ShoppingCart extends Component {
 
 class ProductItem extends Component {
     constructor(product, renderHookId) {
-        super(renderHookId);
+        super(renderHookId, false);
         this.product = product;
+        this.render();
     }
 
     addToCart() {
@@ -106,40 +112,54 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component {
-    products = [
-        new Product(
-            'A Pillow',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPWZ1SG02SGwRyuY3CDSlbR72g8cpRosEwpp2LiCfbbA&s',
-            'A soft pillow.',
-            19.99
-        ),
-        new Product(
-            'A Carpet',
-            'https://cdn.salla.sa/EqRRD/srUEsGVXWXmdyNzyAd1JMLeUoAKZea4cYLA9dXkm.jpg',
-            'A carpet that you might like - or not.',
-            89.99
-        ),
-    ];
+    products = [];
     constructor(renderHookId) {
-        super(renderHookId)
+        super(renderHookId);
+        this.fetchProducts();
     }
+
+    fetchProducts() {
+        this.products = [
+            new Product(
+                'A Pillow',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPWZ1SG02SGwRyuY3CDSlbR72g8cpRosEwpp2LiCfbbA&s',
+                'A soft pillow.',
+                19.99
+            ),
+            new Product(
+                'A Carpet',
+                'https://cdn.salla.sa/EqRRD/srUEsGVXWXmdyNzyAd1JMLeUoAKZea4cYLA9dXkm.jpg',
+                'A carpet that you might like - or not.',
+                89.99
+            ),
+        ];
+        this.renderProducts();
+    }
+
+    renderProducts() {
+        for (const prod of this.products) {
+            new ProductItem(prod, 'prod-list');
+        }
+    }
+
     render() {
         this.createRootElement('ul', 'product-list', [
             new ElementAttribute('id', 'prod-list'),
         ]);
-        for (const prod of this.products) {
-            const productItem = new ProductItem(prod, 'prod-list');
-            productItem.render();
+        if (this.products && this.products.length > 0) {
+            this.renderProducts();
         }
     }
 }
 
 class Shop {
+    constructor() {
+        this.render();
+    }
+
     render() {
         this.cart = new ShoppingCart('app');
-        this.cart.render();
-        const productList = new ProductList('app');
-        productList.render();
+        new ProductList('app');
     }
 }
 
@@ -148,7 +168,6 @@ class App {
 
     static init() {
         const shop = new Shop();
-        shop.render();
         this.cart = shop.cart;
     }
 
