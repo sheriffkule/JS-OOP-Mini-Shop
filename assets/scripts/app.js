460a3811e66b003d27cc85a862baf82fbbf902b2
sheriffkule <zeljko225@yahoo.com>
2024-02-05 09:45:46 +0100

    cart and shop class comunicating & static methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,6 +14,12 @@ class Product {
 
 class ShoppingCart {
     items = [];
+
+    addProduct(product) {
+        this.items.push(product);
+        this.totalOutput.innerHTML = `<h2>Total: \$${12}</h2>`;
+    }
+
     render() {
         const cartEl = document.createElement('section');
         cartEl.innerHTML = `
@@ -21,6 +27,7 @@ class ShoppingCart {
         <button>Order Now!</button>
         `;
         cartEl.className = 'cart';
+        this.totalOutput = cartEl.querySelector('h2');
         return cartEl;
     }
 }
@@ -31,8 +38,7 @@ class ProductItem {
     }
 
     addToCart() {
-        console.log('Adding product to cart...');
-        console.log(this.product);
+        App.addProductToCart(this.product);
     }
 
     render() {
@@ -84,11 +90,12 @@ class ProductList {
 }
 
 class Shop {
+
     render() {
         const renderHook = document.getElementById('app');
 
-        const cart = new ShoppingCart();
-        const cartEl = cart.render();
+        this.cart = new ShoppingCart();
+        const cartEl = this.cart.render();
         const productList = new ProductList();
         const prodListEl = productList.render();
 
@@ -97,5 +104,18 @@ class Shop {
     }
 }
 
-const shop = new Shop;
-shop.render();
+class App {
+    static cart;
+
+    static init() {
+        const shop = new Shop();
+        shop.render();
+        this.cart = shop.cart;
+    }
+
+    static addProductToCart(product) {
+        this.cart.addProduct(product)
+    }
+}
+
+App.init();
